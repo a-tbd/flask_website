78b76de1313cc7c9f7233fca68fa0bac233555c3
itsmeava <immm.ava@gmail.com>
2018-01-13 11:25:27 -0500

    reorganized directory structure

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config(object):
+    DEBUG = True
+    # BASE_DIR = os.path.abspath(os.path.dirname(__file__))
---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+a-tbd presents stuff from my brain
+a home for art, projects, and thoughts from ann tbd (sometimes known as ann kidder, sometimes known as 조혜영)
+"""
+from setuptools import setup
+
+setup(
+    name='a-tbd',
+    version='1.0.0',
+    url='a-tbd.com',
+    author='ann tbd / ann kidder / 조혜영',
+    author_email='ann@a-tbd.com',
+    description='ann\'s website',
+    install_requires=['flask']
+)
